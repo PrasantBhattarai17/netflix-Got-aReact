9f0ec8de5174cc145be2ff4aa65a35ffc9b0d937
notPrasantesp17 <punkersyt@gmail.com>
2024-07-08 21:49:48 +0545

    gettogo

--- src/components/header.js
+++ src/components/header.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='absolute z-10'>
-      <img className='ml-24 p-2 size-[13%]' alt='logo' src='https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png'/>
+    <div className='absolute z-10 bg-gradient-to-b from-black  '>
+      <img className='ml-24 p-4 size-[13%]' alt='logo' src='https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png'/>
     </div>
   )
 }
--- src/components/login.js
+++ src/components/login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Header from './header';
 
+
 const Login = () => {
 
   const [islogedin,setIsLogedIn]=useState(true);
@@ -8,28 +9,40 @@ const Login = () => {
    setIsLogedIn(!islogedin);
   };
   return (
-    <div >
+    <div className='' >
         <Header/>
-        <div className='flex flex-col   '>
-        <div className='absolute  '>
-            <img alt='bg' className='' src='https://assets.nflxext.com/ffe/siteui/vlv3/0552717c-9d8c-47bd-9640-4f4efa2de663/ec7d4963-32a5-46e6-99ab-48d29d11b1a3/NP-en-20240701-POP_SIGNUP_TWO_WEEKS-perspective_WEB_065904e2-cb5a-4a21-8075-d1a67567f76a_large.jpg'/>
+        <div className='flex flex-col    '>
+        <div className='absolute brightness-50   '>
+            <img alt='bg' className=' ' src='https://assets.nflxext.com/ffe/siteui/vlv3/0552717c-9d8c-47bd-9640-4f4efa2de663/ec7d4963-32a5-46e6-99ab-48d29d11b1a3/NP-en-20240701-POP_SIGNUP_TWO_WEEKS-perspective_WEB_065904e2-cb5a-4a21-8075-d1a67567f76a_large.jpg'/>
         </div>
         <div className='relative   mt-40 flex justify-center min-h-96 '>
-            <form className='flex flex-col w-[27%]   justify-center  items-center rounded  bg-black bg-opacity-80 gap-y-4' >
-               <span className='self-start ml-20 py-2 my-2 text-3xl font-bold text-white'>Sign In</span>
-                <input className='border-white border-2 text-white rounded bg-black bg-opacity-70 h-14 p-2 m-2 w-4/6' type='text' placeholder='Enter email or mobile Number'/>
-                <input className='border-white border-2 text-white rounded  bg-black bg-opacity-70 h-14 p-2 m-2 w-4/6' type='password' placeholder='Enter password'/>
-            <button  className='border-white border-1  text-white bg-[red] w-4/6 h-[50px] rounded text-lg font-bold hover:bg-red-700'>{(islogedin)?"Sign in":"Sign up"}</button>
-            <span className='text-white cursor-pointer hover:underline hover:text-gray-400'>{islogedin && "Forgot password?"}</span>
-            <span className='flex items-center self-start ml-20'>
-            <input type='checkbox' className='size-5 '/>
-            <span  className='text-white  font-bold text-md'>Remember me</span>
-            </span>
-            <span className='m-2 p-1 my-4'>
+           {islogedin? <form className='flex flex-col w-[27%]   justify-center  items-center rounded-md  bg-black bg-opacity-80 gap-y-4' >
+           <span className='self-start ml-20 py-2 my-2 text-3xl font-bold text-white'>Sign In</span>
+            <input className='border-gray-400 border-2 text-white rounded bg-black bg-opacity-70 h-14 p-2 m-2 w-4/6' type='text' placeholder='Email or mobile Number'/>
+            <input className='border-gray-400 border-2 text-white rounded  bg-black bg-opacity-70 h-14 p-2 m-2 w-4/6' type='password' placeholder='Password'/>
+        <button  className='border-white border-1  text-white bg-[red] w-4/6 h-[50px] rounded text-lg font-bold hover:bg-red-700'>Sign in</button>
+        <span className='text-white cursor-pointer hover:underline hover:text-gray-400'>Forgot password?</span>
+        <span className='flex items-center self-start ml-20'>
+        <input type='checkbox' className='size-5 '/>
+        <span  className='text-white  font-bold text-md'>Remember me</span>
+        </span>
+        <span className='m-2 p-1 my-4'>
 <span className='text-gray-400 font-sans '>New to Netfilx?</span>
 <span onClick={handleSign} className='text-white font-bold cursor-pointer hover:underline'>Sign Up now</span>
-            </span>
-            </form>
+        </span>
+        </form>:
+            <form className='flex flex-col w-[27%]   justify-center  items-center rounded-md  bg-black bg-opacity-80 gap-y-4' >
+            <span className='self-start ml-20 py-2 my-2 text-3xl font-bold text-white'>Sign Up</span>
+            <input className='border-gray-400 border-2 text-white rounded bg-black bg-opacity-70 h-14 p-2 m-2 w-4/6' type='text' placeholder='Full Name'/>
+           <input className='border-gray-400 border-2 text-white rounded  bg-black bg-opacity-70 h-14 p-2 m-2 w-4/6' type='text' placeholder='Email address'/>
+           <input className='border-gray-400 border-2 text-white rounded  bg-black bg-opacity-70 h-14 p-2 m-2 w-4/6' type='number' placeholder='Mobile number'/>
+           <button  className='border-white border-1  text-white bg-[red] w-4/6 h-[50px] rounded text-lg font-bold hover:bg-red-700'>Sign up</button>
+           <span className='m-2 p-1 my-4'>
+   <span className='text-gray-400 font-sans '>Already registered?</span>
+   <span onClick={handleSign} className='text-white font-bold cursor-pointer hover:underline'>Sign in now</span>
+           </span>
+           </form>
+           }
         </div>
         </div>
     </div>
