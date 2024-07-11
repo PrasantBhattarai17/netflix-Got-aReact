49cadd6cb6bb9e5d54437c29b9c1ec45c171ce4b
notPrasantesp17 <punkersyt@gmail.com>
2024-07-11 09:28:22 +0545

    store created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Body from "./components/body";
 import Header from "./components/header";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 
 function App() {
   return (
     <>
+        <Provider store={appStore}>
     <Header/>
   <Body/>
+  </Provider>
    </>
   );
 }
--- src/components/body.js
+++ src/components/body.js
@@ -14,7 +14,6 @@ const Body = () => {
          }
     ])
   return (
-    
     <RouterProvider router={appRouter}/>
   )
 }
